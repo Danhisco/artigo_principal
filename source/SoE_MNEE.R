dc0e6085f95c9d698946cf2e2cb74d780c31e57e
Danilo Mori <dnlpmo@gmail.com>
2022-11-26 14:33:11 -0300

    atualização da função para incluir o nome do cenário de limitação a dispersão usado

--- source/SoE_MNEE.R
+++ source/SoE_MNEE.R
@@ -1,4 +1,4 @@
-f_SoE_MNEE <- function(df,n_escalas=6,n_replicas=5,landscape_null=FALSE){
+f_SoE_MNEE <- function(df,n_escalas=6,n_replicas=4){
   # estima U para diferentes extensões espaciais da paisagem local, pressupõe um único cenário de dispersão per capita
   # 
   # colunas de df:
@@ -13,7 +13,6 @@ f_SoE_MNEE <- function(df,n_escalas=6,n_replicas=5,landscape_null=FALSE){
     v$U_est
   }
   m_full <- read.table(df$txt.file) |> as.matrix()
-  if(landscape_null) m_full[m_full==0] <- 1
   l_df_U <- list()
   for(i in 1:n_escalas){
     index <- (nrow(m_full)-nrow(m_full)/(2^(i-1)) )/2
@@ -25,5 +24,27 @@ f_SoE_MNEE <- function(df,n_escalas=6,n_replicas=5,landscape_null=FALSE){
                                     lado_km=16.02/(2^(i-1)),p),
                          matrix(v_U,nrow=1))
   }
-  rbind.fill(l_df_U)
+  df_write <- rbind.fill(l_df_U)
+  write_csv(df_write,file = paste0("../csv/SoE/",df$SiteCode,"_k",round(df$k*100,0),".csv"))
+}
+f_SoE_MNEE_null <- function(df,n_escalas=6,n_replicas=4){
+  f_simCoalescente <- function(land_name){
+    v <- dinamica_coalescente(U = 1.25e-06, S=df$S_obs, disp_range = df$d, N_simul = 1,
+                              landscape = land_name)
+    v$U_est
+  }
+  m_full <- read.table(df$txt.file) |> as.matrix()
+  m_full[m_full==0] <- 1
+  l_df_U <- list()
+  for(i in 1:n_escalas){
+    index <- (nrow(m_full)-nrow(m_full)/(2^(i-1)) )/2
+    m_sim <- m_full[(floor(index)+1):(nrow(m_full)-ceiling(index)),
+                    (floor(index)+1):(nrow(m_full)-ceiling(index))]
+    v_U <- replicate(n=n_replicas,f_simCoalescente(land_name = m_sim))
+    l_df_U[[i]] <- cbind(data.frame(SiteCode = df$SiteCode,
+                                    lado_km=16.02/(2^(i-1))),
+                         matrix(v_U,nrow=1))
+  }
+  df_write <- rbind.fill(l_df_U)
+  write_csv(df_write,file = paste0("../csv/SoE/",df$SiteCode,"_k",round(df$k*100,0),".csv"))
 }
