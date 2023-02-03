88fafd097e27fb8325cbef5b9eff7cb9597b5260
Danilo Mori <dnlpmo@gmail.com>
2023-02-03 12:02:25 -0300

    ajustando a delimitação da paisagem local para paisagens cheias não quadradas

--- source/SoE_MNEE.R
+++ source/SoE_MNEE.R
@@ -1,3 +1,4 @@
+# f_SoE_MNEE calcula o p para aquela extensão espacial 
 f_SoE_MNEE <- function(df,n_escalas=6,n_replicas=4){
   # estima U para diferentes extensões espaciais da paisagem local, pressupõe um único cenário de dispersão per capita
   # 
@@ -29,7 +30,8 @@ f_SoE_MNEE <- function(df,n_escalas=6,n_replicas=4){
   df_write <- rbind.fill(l_df_U)
   write_csv(df_write,file = paste0("../csv/SoE/",df$SiteCode,"_k",round(df$k*100,0),".csv"))
 }
-f_SoE_MNEE_null <- function(df,n_escalas=6,n_replicas=4){
+# a versão a seguir não calcula a proporção de cobertura vegetal da paisagem (p), pois é sempre 1
+f_SoE_MNEE_null <- function(df,n_escalas=6,n_replicas=10){
   f_simCoalescente <- function(land_name){
     v <- dinamica_coalescente(U = 1.25e-06, S=df$S_obs, disp_range = df$d, N_simul = 1,
                               landscape = land_name)
@@ -38,9 +40,9 @@ f_SoE_MNEE_null <- function(df,n_escalas=6,n_replicas=4){
   m_full <- read.table(df$txt.file) |> as.matrix()
   l_df_U <- list()
   for(i in 1:n_escalas){
-    index <- (nrow(m_full)-nrow(m_full)/(2^(i-1)) )/2
+    index <- ( nrow(m_full)-(nrow(m_full)/ (2^(i-1))) ) /2
     m_sim <- m_full[(floor(index)+1):(nrow(m_full)-ceiling(index)),
-                    (floor(index)+1):(nrow(m_full)-ceiling(index))]
+                    (floor(index)+1):(ncol(m_full)-ceiling(index))]
     v_U <- replicate(n=n_replicas,f_simCoalescente(land_name = m_sim))
     l_df_U[[i]] <- cbind(data.frame(SiteCode = df$SiteCode,
                                     lado_km=16.02/(2^(i-1)),
