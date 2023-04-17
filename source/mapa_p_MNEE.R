5e948efeb8954518d10ff84cb9f22e0518a74cd1
Danilo Mori <dnlpmo@gmail.com>
2023-04-17 20:29:35 -0300

    trabalho nas simulações auxiliares de MNEE

--- source/mapa_p_MNEE.R
+++ source/mapa_p_MNEE.R
@@ -140,3 +140,25 @@ f_nova_parcela <- function(df){
   #
   return(paisagem)
 }
+
+# função que cria paisagens sem fragmentação per se 'non_frag'
+# source::https://stackoverflow.com/questions/64749481/how-to-solve-a-quadratic-equation-in-r
+f_quad <- function(a, b, c){
+  a <- as.complex(a)
+  answer <- c((-b + sqrt(b^2 - 4 * a * c)) / (2 * a),
+              (-b - sqrt(b^2 - 4 * a * c)) / (2 * a))
+  if(all(Im(answer) == 0)) answer <- Re(answer)
+  if(answer[1] == answer[2]) return(answer[1])
+  answer
+}
+f_nonFragLand <- function(mat){
+  v_0 <- mat[mat==0] |> length()
+  mat[mat==0] <- 1
+  index <- f_2aEqSol(a=-4,b=2*sum(dim(mat)),c = -v_0)[1]
+  i_row <- ceiling(index)
+  i_col <- floor(index)
+  # m_teste <- m_land
+  mat[,c(1:i_col,(ncol(mat)-i_col):ncol(mat))] <- 0
+  mat[c(1:i_row,(nrow(mat)-i_row):nrow(mat)),] <- 0
+  return(mat)
+}
