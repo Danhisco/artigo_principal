d0fa2c736616eb1aa9e5ec25d8b206aba05222fc
Danilo Mori <dnlpmo@gmail.com>
2023-04-18 09:51:32 -0300

    próximo vou mudar o nome para Simulacoes.Rmd

--- source/mapa_p_MNEE.R
+++ source/mapa_p_MNEE.R
@@ -143,7 +143,7 @@ f_nova_parcela <- function(df){
 
 # função que cria paisagens sem fragmentação per se 'non_frag'
 # source::https://stackoverflow.com/questions/64749481/how-to-solve-a-quadratic-equation-in-r
-f_quad <- function(a, b, c){
+f_2aEqSol <- function(a, b, c){
   a <- as.complex(a)
   answer <- c((-b + sqrt(b^2 - 4 * a * c)) / (2 * a),
               (-b - sqrt(b^2 - 4 * a * c)) / (2 * a))
@@ -154,7 +154,7 @@ f_quad <- function(a, b, c){
 f_nonFragLand <- function(mat){
   v_0 <- mat[mat==0] |> length()
   mat[mat==0] <- 1
-  if(v_0>=0.01){
+  if(v_0/length(mat)<=0.01){
     return(mat)
   }
   index <- f_2aEqSol(a=-4,b=2*sum(dim(mat)),c = -v_0)[1]
