c66b8dde95fc99d9d12a377b325192a2e1df2c3c
Danilo Mori <dnlpmo@gmail.com>
2023-05-19 20:29:15 -0300

    modelos para descrever logOR

--- apendices/analise_dados/ajuste_GAMM.R
+++ apendices/analise_dados/ajuste_GAMM.R
@@ -2,11 +2,6 @@ library(readr)
 library(purrr)
 library(stringr)
 library(tidyr)
-library(MuMIn)
-library(AICcmodavg)
-library(insight)
-library(bbmle)
-library(DHARMa)
 library(mgcv)
 library(lme4)
 library(gamm4)
@@ -18,15 +13,33 @@ df_md <- read_csv("dados/csv/df_md_logOR.csv") |>
 f_gam_logOR <- function(df){
   l_md <- list()
   l_md[[1]] <- gam(logOR ~ 
-                     te(p_z,k_z,k=6) +
-                     t2(k_z,SiteCode,k=6,bs=c("cr","re"),full=TRUE),
+                     s(k_z,bs = "cr") + s(p_z,bs = "cr") + ti(p_z,k_z) +
+                     s(k_z, SiteCode, bs="fs",xt=list(bs="cr")),
                    data=df, method="REML")
   l_md[[2]] <- gam(logOR ~ 
-                     te(cubeR_efeito_area_z,cubeR_efeito_frag_z,k=6) +
-                     t2(cubeR_efeito_area_z,cubeR_efeito_frag_z,SiteCode,k=6,bs=c("cr","cr","re"),full=TRUE),
+                     s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") + ti(r3_area_z,r3_frag_z) +
+                     s(r3_frag_z, SiteCode, bs="fs",xt=list(bs="cr")),
                    data=df, method="REML")
-  names(l_md) <- c("~ f(p,k)","~ f(efeito area, efeito frag)")
-  return(l_md)
+  l_md[[3]] <- gam(logOR ~ 
+                     s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") + ti(r3_area_z,r3_frag_z) +
+                     s(r3_area_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                   data=df, method="REML")
+  l_md[[4]] <- gam(logOR ~ 
+                     s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") + ti(r3_area_z,r3_frag_z) +
+                     s(SiteCode,bs="re"),
+                   data=df, method="REML")
+  l_md[[5]] <- gam(logOR ~ 
+                     s(r3_frag_z,bs = "cr") +
+                     s(r3_frag_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                   data=df, method="REML")
+  l_md[[6]] <- gam(logOR ~ 
+                     s(r3_area_z,bs = "cr") +
+                     s(r3_area_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                   data=df, method="REML")
+  names(l_md) <- c("~ f(p, k, k|Site)",
+                   "~ f(area, frag, frag|Site)","~ f(area, frag, area|Site)","~ f(area, frag, 1|Site)",
+                   "~ f(frag, frag|Site)","~ f(area, area|Site)")
+  return(l_md) 
 }
 l_md.logOR <- df_md |> 
   filter(logOR_pair == "cont.non_frag") |> 
