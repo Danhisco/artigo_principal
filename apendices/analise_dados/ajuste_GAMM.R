a0fb2afc64c09678298d5502bb9ae3c96129876c
Danilo Mori <dnlpmo@gmail.com>
2023-05-20 17:53:27 -0300

    versão atual do GAMM para descrever logOR não está adequado, vou seguir a outra estratégia de mudar o tipo de função base

--- apendices/analise_dados/ajuste_GAMM.R
+++ apendices/analise_dados/ajuste_GAMM.R
@@ -14,15 +14,18 @@ f_gam_logOR <- function(df){
   l_md <- list()
   l_md[[1]] <- gam(logOR ~ 
                      s(k_z,bs = "cr") + s(p_z,bs = "cr") + ti(p_z,k_z) +
-                     s(k_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                     s(k_z, SiteCode, bs="fs",xt=list(bs="cr")) +
+                     s(SiteCode,bs="re"),
                    data=df, method="REML")
   l_md[[2]] <- gam(logOR ~ 
                      s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") + ti(r3_area_z,r3_frag_z) +
-                     s(r3_frag_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                     s(r3_frag_z, SiteCode, bs="fs",xt=list(bs="cr")) +
+                     s(SiteCode,bs="re"),
                    data=df, method="REML")
   l_md[[3]] <- gam(logOR ~ 
                      s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") + ti(r3_area_z,r3_frag_z) +
-                     s(r3_area_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                     s(r3_area_z, SiteCode, bs="fs",xt=list(bs="cr")) +
+                     s(SiteCode,bs="re"),
                    data=df, method="REML")
   l_md[[4]] <- gam(logOR ~ 
                      s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") + ti(r3_area_z,r3_frag_z) +
@@ -30,15 +33,28 @@ f_gam_logOR <- function(df){
                    data=df, method="REML")
   l_md[[5]] <- gam(logOR ~ 
                      s(r3_frag_z,bs = "cr") +
-                     s(r3_frag_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                     s(r3_frag_z, SiteCode, bs="fs",xt=list(bs="cr")) +
+                     s(SiteCode,bs="re"),
                    data=df, method="REML")
   l_md[[6]] <- gam(logOR ~ 
                      s(r3_area_z,bs = "cr") +
-                     s(r3_area_z, SiteCode, bs="fs",xt=list(bs="cr")),
+                     s(r3_area_z, SiteCode, bs="fs",xt=list(bs="cr")) +
+                     s(SiteCode,bs="re"),
+                   data=df, method="REML")
+  l_md[[7]] <- gam(logOR ~ 
+                     s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") +
+                     s(r3_frag_z, SiteCode, bs="fs",xt=list(bs="cr")) +
+                     s(SiteCode,bs="re"),
+                   data=df, method="REML")
+  l_md[[8]] <- gam(logOR ~ 
+                     s(r3_area_z,bs = "cr") + s(r3_frag_z,bs = "cr") +
+                     s(r3_area_z, SiteCode, bs="fs",xt=list(bs="cr")) +
+                     s(SiteCode,bs="re"),
                    data=df, method="REML")
   names(l_md) <- c("~ f(p, k, k|Site)",
                    "~ f(area, frag, frag|Site)","~ f(area, frag, area|Site)","~ f(area, frag, 1|Site)",
-                   "~ f(frag, frag|Site)","~ f(area, area|Site)")
+                   "~ f(frag, frag|Site)","~ f(area, area|Site)",
+                   "~ f(area,frag,frag|Site - ti","~ f(area,frag,area|Site - ti")
   return(l_md) 
 }
 l_md.logOR <- df_md |> 
