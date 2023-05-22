002aacc60a3f45343288408748c708531d322a77
Danilo Mori <dnlpmo@gmail.com>
2023-05-21 23:11:42 -0300

    GAMM com os efeitos na escala base, sem ser na raiz cúbica, apresentam ajuste um pouco melhor, porém, há muita variação que o modelo não explica. Vou avaliar o predito para novo conjunto de dados

--- apendices/analise_dados/ajuste_GAMM.R
+++ apendices/analise_dados/ajuste_GAMM.R
@@ -8,8 +8,13 @@ library(gamm4)
 library(plyr)
 library(dplyr)
 df_md <- read_csv("dados/csv/df_md_logOR.csv") |> 
-  mutate(SiteCode = factor(SiteCode))
-#
+  mutate(SiteCode = factor(SiteCode)) |> 
+  filter(logOR_pair == "cont.non_frag")
+# não há capacidade computacional suficiente para rodar:
+# md.logOR_t2full <- gam(logOR ~ 
+#                          te(area_z,frag_z,k=15,bs=c("tp","tp"),m=2) +
+#                          t2(area_z,frag_z,SiteCode,k=6,bs=c("tp","tp","re"),full=TRUE,m=2),
+#                        data=df, method="REML")
 f_gam_logOR <- function(df){
   l_md <- list()
   l_md[[1]] <- gam(logOR ~ 
@@ -17,8 +22,8 @@ f_gam_logOR <- function(df){
                      s(k_z, SiteCode, bs="fs",xt=list(bs="tp")) +
                      s(SiteCode,bs="re"),
                    data=df, method="REML")
-  md_logOR <- gam(logOR ~ 
-                     s(area_z,bs = "tp") + s(frag_z,bs = "tp") + ti(area_z,frag_z,bs=c("tp","tp")) +
+  l_md[[2]] <- gam(logOR ~ 
+                     s(area_z,bs = "tp") + s(frag_z,bs = "tp") + ti(area_z,frag_z) +
                      s(frag_z, SiteCode, bs="fs",xt=list(bs="tp")) +
                      s(SiteCode,bs="re"),
                    data=df, method="REML")
@@ -57,7 +62,14 @@ f_gam_logOR <- function(df){
                    "~ f(area,frag,frag|Site - ti","~ f(area,frag,area|Site - ti")
   return(l_md) 
 }
-l_md.logOR_r3remov <- df_md |> 
-  filter(logOR_pair == "cont.non_frag") |> 
+l_md.logOR_tp_r3removido <- df_md |> 
   f_gam_logOR()
-save(l_md.logOR_r3remov,file="dados/Rdata/l_md.logOR_r3removido.Rdata")
+save(l_md.logOR_tp_r3removido,file="dados/Rdata/l_md.logOR_tp_r3removido.Rdata")
+
+#erro: modelo possui mais coeficientes do que dados:
+# l_md[[9]] <- gam(logOR ~ 
+#                    s(area_z,bs = "tp") + s(frag_z,bs = "tp") + ti(area_z,frag_z) +
+#                    s(frag_z, SiteCode, bs="fs",xt=list(bs="tp")) +
+#                    s(area_z, SiteCode, bs="fs",xt=list(bs="tp")) +
+#                    s(SiteCode,bs="re"),
+#                  data=df, method="REML")
