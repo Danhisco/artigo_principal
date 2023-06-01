f5c8af23e346c8051c28d3019e1f6b15847dc470
Danilo Mori <dnlpmo@gmail.com>
2023-06-01 14:08:02 -0300

    funções para gerar os gráficos finais dos GAMMs 1d

--- source/GAMMtools.R
+++ source/GAMMtools.R
@@ -14,4 +14,132 @@ f_TabSelGAMM <- function(l_md){
 }
 #
 # 
+## gamm: um mgcv::gam
+## data: df com colunas com as preditoras do gamm
+## v_exclude: quais os splines devem ser desconsiderados?
+## quantiles: intervalo de predição retornado
+## nsim: número de amostras da distribuição normal multivariada dos coef estimados pelo gamm
+#
+# output: retorna um data frame com o intevalo de predição na escala da função de ligação e as preditoras
+f_PredInt.GAMM <- \(gamm,
+                    data, 
+                    v_exclude = c("s(k_z,SiteCode)","s(SiteCode)"),
+                    quantiles = c(0.05,0.25,0.5,0.75,0.95),
+                    nsim=10000){
+  data$predito <- predict(gamm, type = "link",
+                          exclude = v_exclude,
+                          newdata=data,
+                          newdata.guaranteed=TRUE) |> as.vector()
+  beta <- coef(gamm)
+  V <- vcov.gam(gamm)
+  num_beta_vecs <- nsim
+  Cv <- chol(V)
+  set.seed(1)
+  nus <- rnorm(num_beta_vecs * length(beta))
+  beta_sims <- beta + t(Cv) %*% matrix(nus, nrow = length(beta), ncol = num_beta_vecs)
+  matrix_lprediction <- predict(gamm,type="lpmatrix",
+                                exclude = c("s(k_cont_z,SiteCode)","s(SiteCode)"),
+                                newdata=data,newdata.guaranteed=TRUE) 
+  predict_link <- matrix_lprediction %*% beta_sims
+  df_pred <- t(apply(predict_link,1,\(X) quantile(X,probs = quantiles))) %>% 
+    as.data.frame()
+  names(df_pred) <- paste0("Q_",quantiles)
+  cbind(data,df_pred)
+}
+#
+#
+## posterior prediction interval from a GAMM
+# input: GAMM
+# output: a ggplot2 scatter plot with the observed values and predictions;
+# and predictions without variability between sites.
+f_PostPredPlotGAMM1d_obsEpred <- \(gamm,
+                                   site.posteriori ="SPigua1",
+                                   length.out_x = 200,
+                                   n.posteriori_samples = 10000){
+  # take the GAMM objects:
+  f_invlink <- gamm$family$linkinv
+  v_devexp <- summary(gamm)$dev.exp
+  if(gamm$family$family=="binomial"){
+    df_obs <- gamm$model |> select(-1) |> 
+      mutate(y = gamm$model[,1][,1]) |> relocate(y)
+    names(df_obs)[1] <- colnames(gamm$model[,1])[1]  
+  }else{
+    df_obs <- gamm$model
+  }
+  # create the new data 
+  y_var <- names(df_obs)[1]
+  x_var <- names(df_obs)[2]
+  df_newpred <- data.frame(x = seq(min(df_obs[,x_var]),
+                                   max(df_obs[,x_var]),
+                                   length.out=length.out_x)) |> 
+    mutate(SiteCode = site.posteriori)
+  names(df_newpred)[1] <- x_var
+  # obtain the predictions
+  ## new predictions without variability between sites
+  df_pred_newdata <- f_PredInt.GAMM(data=df_newpred,
+                                    gamm=gamm,
+                                    nsim=n.posteriori_samples,
+                                    v_exclude=c(paste0("s(",x_var,",SiteCode)"),
+                                                "s(SiteCode)"))
+  ## predictions for the observed data (with variability between sites)
+  df_obs$predito <- predict(gamm)
+  ## apply the inverse link function
+  ### if the case is a binomial GAMM
+  if(gamm$family$family=="binomial"){
+    df_pred_newdata <- df_pred_newdata |> 
+      mutate(across(predito:8,\(x) f_invlink(x) * sum(gamm$model[1,1])))
+    df_obs <- df_obs |> 
+      mutate(predito = f_invlink(predito) * sum(gamm$model[1,1]))  
+  }else{
+    df_pred_newdata <- df_pred_newdata |> 
+      mutate(across(predito:8,f_invlink))
+    df_obs <- df_obs |> 
+      mutate(predito = f_invlink(predito))  
+  }
+  # create the final figure
+  df_obs |> 
+    ggplot(aes(x=.data[[x_var]],y=.data[[y_var]])) +
+    geom_point(alpha=0.2) +
+    geom_line(aes(y=predito,group=SiteCode),alpha=0.2) +
+    geom_line(data=df_pred_newdata,
+              aes(x=.data[[x_var]],y=Q_0.5),color="darkred") +
+    geom_line(data = df_pred_newdata |> 
+                select(-Q_0.5) |> 
+                pivot_longer(starts_with("Q_0.")),
+              aes(x=.data[[x_var]],y=value,group=name),color="darkblue") +
+    labs(y = "congruência",
+         subtitle = paste0("full model dev. exp. = ",round(v_devexp*100,digits = 1),"%")) +
+    geom_vline(xintercept = quantile(df_obs[,x_var],probs = c(0.95,0.99)),linetype="dashed",alpha=0.3) +
+    theme_bw() +
+    theme(plot.caption = element_text(hjust=0))
+}
+#
+#
+## função para customizar o gráfico produzido por f_PostPredPlotGAMM1d_obsEpred
+# adiciona título e rótulo do eixo x
+f_l_mdGAMM_FiguraFinal_1dGAMM <- \(l_md){
+  df_labels <- data.frame(pair = names(l_md)) |> 
+    mutate(contraste = case_when(pair == "cont.ideal" ~ "contemporaneidade",
+                                 pair == "cont.non_frag" ~ "fragmentação per se",
+                                 TRUE ~ "área per se"),
+           labels = case_when(pair == "cont.ideal" ~ "contemp. : ideal.",
+                              pair == "cont.non_frag" ~ "contemp. : sem frag.",
+                              TRUE ~ "sem frag. : ideal."))
+  
+  l_p <- llply(l_md,f_PostPredPlotGAMM1d_obsEpred)
+  names(l_p) <- names(l_md)
+  for(plot in names(l_p)){
+    l_p[[plot]] <- l_p[[plot]] +
+      labs(x=df_labels |> 
+             filter(pair == plot) |> 
+             pull(contraste),
+           title=df_labels |> 
+             filter(pair == plot) |> 
+             pull(labels)) +
+      theme(plot.title = element_text(hjust = 0.5,
+                                      size = 12, 
+                                      face = "bold"))
+  }
+  return(l_p)
+}
 
