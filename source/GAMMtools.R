3228c43b560963945f3e99ceee0bc1bfcd37abad
Danilo Mori <dnlpmo@gmail.com>
2023-08-04 17:30:46 -0300

    figura 3 congruencia entre contrastes FEITA! Aqui é super sayajin jedi

--- source/GAMMtools.R
+++ source/GAMMtools.R
@@ -40,7 +40,7 @@ f_validaGAMM <- \(md_name,l_md,size=0){
 # f_PredInt.GAMM
 ## gamm: um mgcv::gam
 ## data: df com colunas com as preditoras do gamm
-## v_exclude: quais os splines devem ser desconsiderados?
+## v_exclude: quais os splines devem ser desconsiderados? em geral desconsidero a varib. entre sítios
 ## quantiles: intervalo de predição retornado
 ## nsim: número de amostras da distribuição normal multivariada dos coef estimados pelo gamm
 #
@@ -72,7 +72,8 @@ f_PredInt.GAMM <- \(gamm,
   cbind(data,df_pred)
 }
 #
-# f_calcPI
+# f_calcPI: 
+# aplica f_PredInt.GAMM para um GAMM de dois tipos: ~ contraste_z ou ~ k,p
 f_calcPI <- \(gamm,
               site.posteriori ="SPigua1",
               length_pred = 150,
@@ -90,32 +91,24 @@ f_calcPI <- \(gamm,
     if(any(names(df_obs) |> str_detect("contraste_z"))){
       y_var <- names(df_obs)[1]
       x_var <- names(df_obs)[2]
-      df_newpred <- data.frame(x = seq(min(df_obs[,x_var]),
-                                       max(df_obs[,x_var]),
-                                       length.out=length_pred)) |> 
+      df_newpred <- data.frame(x = seq(min(df_obs[,x_var]),max(df_obs[,x_var]),length.out=length_pred)) |> 
         mutate(SiteCode = site.posteriori)
       names(df_newpred)[1] <- x_var  
     }else{
       y_var <- names(df_obs)[1]
       x1_var <- names(df_obs)[2]
       x2_var <- names(df_obs)[3]
-      df_newpred <- expand.grid(x1 = seq(min(df_obs[,x1_var]),
-                                         max(df_obs[,x1_var]),
-                                         length.out=length_pred),
-                                x2 = seq(min(df_obs[,x2_var]),
-                                         max(df_obs[,x2_var]),
-                                         length.out=length_pred)) |> 
+      df_newpred <- expand.grid(x1 = seq(min(df_obs[,x1_var]),max(df_obs[,x1_var]),length.out=length_pred),
+                                x2 = seq(min(df_obs[,x2_var]),max(df_obs[,x2_var]),length.out=length_pred)) |> 
         mutate(SiteCode = site.posteriori)
       names(df_newpred)[1:2] <- c(x1_var,x2_var)
       x_var <- "k_z"
     }
     # obtain the predictions
     ## new predictions without variability between sites
-    df_newpred <- f_PredInt.GAMM(data=df_newpred,
-                                      gamm=gamm,
-                                      nsim=n.posteriori_samples,
-                                      v_exclude=c(paste0("s(",x_var,",SiteCode)"),
-                                                  "s(SiteCode)"))
+    df_newpred <- f_PredInt.GAMM(data=df_newpred,gamm=gamm,
+                                 nsim=n.posteriori_samples,
+                                 v_exclude=c(paste0("s(",x_var,",SiteCode)"),"s(SiteCode)"))
     ## predictions for the observed data (with variability between sites)
     ## apply the inverse link function
     ### if the case is a binomial GAMM
@@ -128,17 +121,103 @@ f_calcPI <- \(gamm,
     }
     return(df_newpred)
 }
+# f_ggplot_PI1d:
+# padroniza um scatterplot simples para f_plotCongCont
+f_ggplot_PI1d <- \(df_pred,df_obs,x_var,y_var,title_contraste){
+  df_obs |> 
+    ggplot(aes(x=.data[[x_var]],y=.data[[y_var]])) +
+    geom_point(alpha=0.2) +
+    geom_line(aes(y=predito,group=SiteCode),alpha=0.2) +
+    geom_line(data=df_pred,
+              aes(x=.data[[x_var]],y=Q_0.5),color="darkred") +
+    geom_line(data = df_pred |> 
+                select(-Q_0.5) |> 
+                pivot_longer(starts_with("Q_0.")),
+              aes(x=.data[[x_var]],y=value,group=name),color="darkblue") +
+    labs(y = "congruência", x = "z(contraste da taxa U)") +
+    ggtitle(title_contraste) +
+    geom_vline(xintercept = quantile(df_obs[[x_var]],probs = c(0.95,0.99)),linetype="dashed",alpha=0.3) +
+    theme_bw() +
+    theme(plot.title.position = "plot")
+}
+# f_ggplot_PI2d:
+# padroniza um heatamp para f_plotCongCont
+f_ggplot_PI2d <- \(df,v_range,v_breaks=NULL,title_contraste){
+  f_ggplot <- \(df,facets=2){
+    ggplot(df,aes(x=p,y=k,z=pred,fill=pred)) +
+      geom_raster() +
+      geom_contour(aes(z=pred),
+                   size=0.5,color="darkblue",
+                   breaks=v_breaks) +
+      geom_text_contour(aes(z=pred),
+                        breaks=v_breaks,
+                        label.placer = label_placer_flattest()) +
+      # scale_fill_gradient2(midpoint = median(v_range),
+      #                      limits=v_range) +
+      scale_fill_gradientn(colours=c("#0000FFFF","#FFFFFFFF","#FF0000FF")) +
+      scale_y_reverse() +
+      theme_classic() +
+      # guides(fill = guide_colourbar(title = ) +
+      coord_cartesian(expand = FALSE) +
+      labs(fill="Prob. Cong. Pred.") +
+      facet_wrap(~label,ncol = facets,scales="free",
+                 labeller = as_labeller(v_labels))
+  }
+  v_labels <- c("Q_0.05"="0.05",
+                "Q_0.25"="0.25",
+                "Q_0.5"="median",
+                "Q_0.75"="0.75",
+                "Q_0.95"="0.95")
+  l_p <- list()
+  l_p[[1]] <- df |> 
+    filter(label == "Q_0.5") |>
+    f_ggplot() + labs(x="")
+  l_p[[2]] <- df |> 
+    filter(label != "Q_0.5") |>
+    f_ggplot()
+  ggpubr::ggarrange(plotlist = l_p,nrow=2, common.legend = TRUE, legend="bottom") |> 
+    ggpubr::annotate_figure(top = ggpubr::text_grob(title_contraste,hjust=0,x=0))
+}
+# f_titleContraste
+# padroniza os títulos dos gráficos para f_plotCongCont
+f_titleContraste <- \(string){
+  if(string=="cont.ideal"){
+    "a) Cont. : Ideal."
+  }else if(string=="cont.non_frag"){
+    "b) Cont. : Sem Frag."
+  }else if(string=="non_frag.ideal"){
+    "c) Sem Frag. : Ideal."
+  }
+}
+# f_plotCongCont
+# função que gera os gráficos finais da congruência das SADs preditas entre contrastes
+# plota a predição dos GAMM mais plausíveis segundo tabela 1 dos resultados
+f_plotCongCont <- \(df){
+  df_pred <- read_csv(paste0("./dados/csv/df_PIcongCont",df$pair[1],".csv"))
+  if(any(names(df_pred)=="contraste_z")){
+    df$predito <- predict.gam(object = l_md_congContrastes[[df$pair[1]]][["f(contraste_z)"]],
+                              type = "response") * 500
+    title_contraste <- f_titleContraste(df$pair[1])
+    f_ggplot_PI1d(df_pred,df,"contraste_z","nCong",title_contraste = title_contraste)
+  }else{
+    df_pred <- df_pred |> 
+      mutate(p = p_z*sd(df_ad$p) + mean(df_ad$p),
+             k = k_z*sd(df_ad$k) + mean(df_ad$k)) |> 
+      pivot_longer(starts_with("Q_0."),values_to = "pred",names_to = "label")
+    df_pred$label <- factor(df_pred$label,levels = unique(df_pred$label)[c(3,2,4,1,5)])
+    # gráficos
+    v_range <- range(df_pred$pred)
+    title_contraste <- f_titleContraste(df$pair[1])
+    f_ggplot_PI2d(df=df_pred,title_contraste = title_contraste)
+  }
+}
 
 #
-# f_PostPredPlotGAMM1d_obsEpred
-## posterior prediction interval from a GAMM
-# input: GAMM
-# output: a ggplot2 scatter plot with the observed values and predictions;
-# and predictions without variability between sites.
-f_PostPredPlotGAMM1d_obsEpred <- \(gamm,
-                                   site.posteriori ="SPigua1",
-                                   length.out_x = 200,
-                                   n.posteriori_samples = 10000){
+# deprecated funcions:
+d_f_PostPredPlotGAMM1d_obsEpred <- \(gamm,
+                                     site.posteriori ="SPigua1",
+                                     length.out_x = 200,
+                                     n.posteriori_samples = 10000){
   # take the GAMM objects:
   f_invlink <- gamm$family$linkinv
   v_devexp <- summary(gamm)$dev.exp
@@ -196,13 +275,10 @@ f_PostPredPlotGAMM1d_obsEpred <- \(gamm,
     theme_bw() +
     theme(plot.caption = element_text(hjust=0))
 }
-#
-# f_PostPredPlotGAMM1d1f_obsEpred
-## função para post PI 1d 1f # pensar em formas 
-f_PostPredPlotGAMM1d1f_obsEpred <- \(gamm,
-                                     site.posteriori ="SPigua1",
-                                     length.out_x = 200,
-                                     n.posteriori_samples = 10000){
+d_f_PostPredPlotGAMM1d1f_obsEpred <- \(gamm,
+                                       site.posteriori ="SPigua1",
+                                       length.out_x = 200,
+                                       n.posteriori_samples = 10000){
   # take the GAMM objects:
   f_invlink <- gamm$family$linkinv
   v_devexp <- summary(gamm)$dev.exp
@@ -229,10 +305,10 @@ f_PostPredPlotGAMM1d1f_obsEpred <- \(gamm,
         select(where(is.factor)) |> 
         pull() |> 
         unique()
-      ) |> 
+    ) |> 
       mutate(SiteCode = factor(site.posteriori))
     names(df_newpred) <- c(x_var,v_other.factor,"SiteCode")
-    }else{
+  }else{
     df_newpred <- data.frame(x = seq(min(df_obs[,x_var]),
                                      max(df_obs[,x_var]),
                                      length.out=length.out_x)) |> 
@@ -280,11 +356,7 @@ f_PostPredPlotGAMM1d1f_obsEpred <- \(gamm,
     theme(plot.caption = element_text(hjust=0)) +
     facet_wrap(~.data[[v_other.factor]],nrow=1)
 }
-#
-# f_l_mdGAMM_FiguraFinal_1dGAMM
-## função para customizar o gráfico produzido por f_PostPredPlotGAMM1d_obsEpred
-# adiciona título e rótulo do eixo x
-f_l_mdGAMM_FiguraFinal_1dGAMM <- \(l_md){
+d_f_l_mdGAMM_FiguraFinal_1dGAMM <- \(l_md){
   df_labels <- data.frame(pair = names(l_md)) |> 
     mutate(contraste = case_when(pair == "cont.ideal" ~ "contemporaneidade",
                                  pair == "cont.non_frag" ~ "fragmentação per se",
@@ -309,68 +381,3 @@ f_l_mdGAMM_FiguraFinal_1dGAMM <- \(l_md){
   }
   return(l_p)
 }
-
-# ggplo2 para plootar intervalo de predição 1d com os valores observados
-f_ggplot_PI1d <- \(df_pred,df_obs,x_var,y_var){
-  df_obs |> 
-    ggplot(aes(x=.data[[x_var]],y=.data[[y_var]])) +
-    geom_point(alpha=0.2) +
-    geom_line(aes(y=predito,group=SiteCode),alpha=0.2) +
-    geom_line(data=df_pred,
-              aes(x=.data[[x_var]],y=Q_0.5),color="darkred") +
-    geom_line(data = df_pred |> 
-                select(-Q_0.5) |> 
-                pivot_longer(starts_with("Q_0.")),
-              aes(x=.data[[x_var]],y=value,group=name),color="darkblue") +
-    labs(y = "congruência",
-         subtitle = paste0("full model dev. exp. = ",round(v_devexp*100,digits = 1),"%")) +
-    geom_vline(xintercept = quantile(df_obs[,x_var],probs = c(0.95,0.99)),linetype="dashed",alpha=0.3) +
-    theme_bw() +
-    theme(plot.caption = element_text(hjust=0))
-}
-# ggplot2 para plotar intervalo de predição 2D
-f_ggplot_PI2d <- \(df,v_range,v_breaks1=c(-0.2,0,0.2,0.5,1)){
-  f_ggplot <- \(df,facets=2){
-    ggplot(df,aes(x=p,y=k,z=pred,fill=pred)) +
-      geom_raster() +
-      geom_contour(aes(z=pred),
-                   size=0.5,color="darkblue",
-                   breaks=v_breaks1) +
-      geom_text_contour(aes(z=pred),
-                        breaks=v_breaks1,
-                        label.placer = label_placer_flattest()) +
-      scale_fill_gradient2(low="green",
-                           mid="red",
-                           high="black",
-                           midpoint = 0.5,
-                           limits=v_range,
-                           breaks=v_breaks1) +
-      scale_y_reverse() +
-      theme_classic() +
-      guides(fill = guide_colourbar(title = gsub("efeito_","",df$name[1]))) +
-      coord_cartesian(expand = FALSE) +
-      labs(fill=df$name[1]) +
-      facet_wrap(~label,ncol = facets,scales="free")
-  }
-  l_p <- list()
-  l_p[[1]] <- df |> 
-    filter(label == "Q_0.5") |>
-    f_ggplot() + labs(x="")
-  l_p[[2]] <- df |> 
-    filter(label != "Q_0.5") |>
-    f_ggplot()
-  ggpubr::ggarrange(plotlist = l_p,nrow=2, common.legend = TRUE, legend="top")
-}
-#
-#
-f_plotCongCont <- \(df){
-  df_pred <- read_csv(paste0("./dados/csv/df_PIcongCont",df$pair[1],".csv"))
-  if(any(names(df_pred)=="contraste_z")){
-    df$predito <- predict.gam(object = l_md_congContrastes[[df$pair[1]]][["f(contraste_z)"]],
-                              type = "response")
-      
-  }else{
-    
-  }
-}
-
