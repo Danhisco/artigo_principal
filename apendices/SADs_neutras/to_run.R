e0d1884fc589a10f7d43155b691dd8e62ddcd191
Danilo Pereira Mori <dnlpmo@gmail.com>
2024-01-24 07:05:53 -0300

    criação de df_repRes2.csv

--- apendices/SADs_neutras/to_run.R
+++ apendices/SADs_neutras/to_run.R
@@ -1,5 +1,6 @@
 # funções
 library(twosamples)
+library(doMC)
 library(sads)
 library(readr)
 library(purrr)
@@ -8,7 +9,7 @@ library(tidyr)
 library(plyr)
 library(dplyr)
 source("source/2samples_testes.R")
-# dados
+# rotinas
 if(!file.exists("dados/csv/resultados_MN/MNEE/df_repMNEE2.csv")){
   df_SADrep2 <- mutate(data.frame(
     SADrep.path = list.files(path = "dados/csv/SADs_neutras/MNEE_taxaU_idealizado",
@@ -16,15 +17,14 @@ if(!file.exists("dados/csv/resultados_MN/MNEE/df_repMNEE2.csv")){
     SiteCode = str_extract(SADrep.path,"(?<=\\_\\_).*?(?=.csv)"),
     land_type = str_extract(SADrep.path,"(?<=zado\\/).*?(?=\\_\\_)"),
     SADobs.path = paste0("dados/csv/SADs_observadas/",SiteCode,".csv"))
-# rotina
   registerDoMC(3)
   df_repMNEE <- adply(df_SADrep2,1,f_resultsMN,.parallel = TRUE)
   write_csv(df_repMNEE, file = "dados/csv/resultados_MN/MNEE/df_repMNEE2.csv")  
 }else if(file.exists("./dados/csv/resultados_MN/MNEE/df_repMNEE2.csv")){
   registerDoMC(3)
-  df_repMNEE <- read_csv("./dados/csv/resultados_MN/MNEE/df_repMNEE")
+  df_repMNEE <- read_csv("./dados/csv/resultados_MN/MNEE/df_repMNEE2.csv")
   df_repRes <- ddply(df_repMNEE,c("SiteCode","k","land_type"),f_summarise_SAD_MNEE,.parallel = TRUE)
   write_csv(df_repRes, file = "dados/csv/resultados_MN/MNEE/df_repRes2.csv")  
 }else{
-  warning("Tem trabalho para fazer? Se sim, remover os .csv ou mudar código")
+  warning("Tem trabalho para fazer? Se sim, remover os .csv")
 }
