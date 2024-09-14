3d9634f2a60493e3127c60416c8a8e2349ef2af3
Danilo Mori <dnlpmo@gmail.com>
2024-09-14 18:06:22 -0300

    remoção do ajuste de GAMM com as covariáveis

--- 0_apendices/analise_dados/ajuste_GAMM.R
+++ 0_apendices/analise_dados/ajuste_GAMM.R
@@ -3,6 +3,7 @@ source("source/2samples_testes.R")
 source("source/general_tools.R")
 source("source/GAMMtools.R")
 source("source/fig_tools.R")
+library(mgcv)
 v_path <- "/home/danilo/Documentos/mestrado_Ecologia/artigo_principal/1_to_compile_dissertacao_EM_USO/00_Resultados/"
 ###########################################################################
 ###### Como expressar o efeito da paisagem por sítio de amostragem?  ######
@@ -52,119 +53,12 @@ f_gam <- \(dfi){
     data=dfi,method = "REML")
   return(l_md)
 }
-
 l_md_logOR <- dlply(df_md,"contraste",f_gam)
 saveRDS(l_md_logOR,file=paste0(v_path,"rds/l_md_simples_apudPedersen2019.rds"))
 # l_md_logOR <- readRDS(file=paste0(v_path,"rds/l_md_simples.rds"))
 df_tabelaSelecao <- ldply(l_md_logOR,f_TabSelGAMM,.id="pair")
 write_csv(df_tabelaSelecao,
           file=paste0(v_path,"rds/tabsel_simples.csv"))
-
-
-
-####
-saveRDS(l_md_logOR,file=paste0(v_path,"rds/l_md_1aperg_efeito_paisagem_p_sitio.rds"))
-## tabela de seleção
-# l_md_logOR <- readRDS(file=paste0(v_path,"rds/l_md_1aperg_efeito_paisagem_p_sitio.rds"))
-## tabela de selação dos modelos 
-df_tabelaSelecao <- ldply(l_md_logOR,f_TabSelGAMM,.id="pair")
-write_csv(df_tabelaSelecao,
-          file=paste0(v_path,"rds/tabsel_1aperg_efeito_paisagem_p_sitio.csv"))
-rm(list="l_md_logOR");gc()
-##########################################################
-############# Qual o modelo cheio suficiente? ############
-##########################################################
-# objectos comuns
-# modelos mais plausíveis da seleção passada:
-# f_SML.gamm <- \(df){md <- l_md_logOR[[df$contraste[1]]][[df$modelo[[1]]]]}
-# l_md_1alike <- dlply(df_tabelaSelecao,"contraste",f_SML.gamm)
-## modelos usados
-## função de atualização do modelo cheio mais plausível
-f_gam_selmodcheio <- \(dfi){ # uma vez que todos faoram selecionados para ser
-  # formula 
-  l_md <- list()
-  l_md$`efeito fixo por preserv` <- 
-    gam(
-      logOR ~
-        forest_succession +
-        s(Uefeito, by = forest_succession, bs = "cr", id = "effect_forest") +
-        s(data_year, bs = "cr") +
-        s(lat, long, bs = "tp") +
-        s(SiteCode, bs = "re") +
-        s(Uefeito, SiteCode, bs = "fs", xt = list(bs = "cr"), id = "effect_site"),
-      data = dfi, method = "REML")
-  l_md$`efeito fixo comum` <- 
-    gam(
-      logOR ~
-        forest_succession +
-        s(Uefeito, bs = "cr", id = "effect_forest") +
-        s(data_year, bs = "cr") +
-        s(lat, long, bs = "tp") +
-        s(SiteCode, bs = "re") +
-        s(Uefeito, SiteCode, bs = "fs", xt = list(bs = "cr"), id = "effect_site"),
-      data = dfi, method = "REML")
-  return(l_md)
-}
-l_md_logOR <- dlply(df_md,"contraste",f_gam_selmodcheio)
-saveRDS(l_md_logOR,file=paste0(v_path,"rds/l_md_2aperg_qual_md_cheio.rds"))
-##########################################################
-######### Qual o melhor conjunto de covariáveis? #########
-##########################################################
-# objetos necessários
-df_tabelaSelecao <- ldply(l_md_logOR,f_TabSelGAMM,.id="pair")
-write_csv(df_tabelaSelecao,
-          file=paste0(v_path,"rds/tabsel_2aperg_qual_md_cheio.csv"))
-f_SML.gamm <- \(df){md <- l_md_logOR[[df$contraste[1]]][[df$modelo[[1]]]]}
-l_md_1alike <- dlply(df_tabelaSelecao,"contraste",f_SML.gamm)
-f_gam_update <- \(mdname,
-                  name_path = paste0(v_path,"rds/l_md_3aperg_quais_cov_")){
-  mdgam <- l_md_1alike[[mdname]]
-  f_ref <- formula(mdgam)
-  original_data <- mdgam$model
-  f_gam <- \(fi){
-    gam(formula=fi,data=original_data,method = "REML")
-  }
-  l_md <- list()
-  l_md$cheio <- f_ref
-  l_md$`- ano` <- update.formula(f_ref, . ~ . - s(data_year, bs = "cr"))
-  l_md$`- coord` <- update.formula(f_ref, . ~ . - s(lat, long, bs = "tp"))
-  if(!grepl("by = forest_succession",as.character(f_ref)[3])){
-    l_md$`- pert` <- update.formula(f_ref, . ~ . - forest_succession)
-    l_md$`- pert e ano` <- update.formula(l_md$`- ano`, . ~ . - forest_succession)
-    l_md$`- pert e coord` <- update.formula(l_md$`- coord`, . ~ . - forest_succession)
-    l_md$`- ano e coord` <- update.formula(l_md$`- coord`, . ~ . - s(data_year, bs = "cr"))
-    l_md$`- cov` <- update.formula(l_md$`- ano e coord`, . ~ . - forest_succession)
-  }else{
-    l_md$`- cov` <- update.formula(l_md$`- coord`, . ~ . - s(data_year, bs = "cr"))
-  }
-  l_md <- lapply(l_md,f_gam)
-  mdname <- gsub("Área per se","areaperse",mdname) %>% 
-    gsub("Frag. per se","fragperse",.) %>% 
-    gsub("Frag. total","fragtotal",.)
-  saveRDS(l_md,
-          file=paste0(name_path,mdname,".rds"))
-  rm(l_md);gc()
-}
-lapply(names(l_md_1alike),f_gam_update)
-## tabelas de seleção
-paths <- list.files(path=gsub("/l_md_3aperg_quais_cov_","",
-                              eval(formals(f_gam_update)$name_path)),
-                    pattern="3aperg",full.names = T)
-df_tabelaSelecao <- lapply(paths,\(li){
-  l_md <- readRDS(li)
-  df_return <- f_TabSelGAMM(l_md)
-  mutate(df_return,
-         contraste=str_extract(li,"(?<=cov\\_)(.*?)(?=\\.rds)") %>% 
-           gsub("areaperse","Área per se",.) %>% 
-           gsub("fragperse","Frag. per se",.) %>% 
-           gsub("fragtotal","Frag. total",.))
-}) %>% do.call("rbind",.)
-write_csv(df_tabelaSelecao,
-          file=paste0(v_path,"rds/tabsel_3aperg_quais_cov.csv"))
-############################################
-#
-#
-############################################
 ###  tabelas e diagnósticos
 l_md <- readRDS(paste0(v_path,"rds/l_md_simples.rds"))
 df_tabsel <- read_csv(paste0(v_path,"rds/tabsel_simples.csv")) %>%
