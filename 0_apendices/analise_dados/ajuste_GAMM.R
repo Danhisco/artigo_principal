b2279a51f7c0af061808cc277b66baf4e9bbb596
Danilo Mori <dnlpmo@gmail.com>
2024-12-30 09:17:14 -0300

    modelos HGAMs ajustados

--- 0_apendices/analise_dados/ajuste_GAMM.R
+++ 0_apendices/analise_dados/ajuste_GAMM.R
@@ -19,29 +19,12 @@ source("source/general_tools.R")
 source("source/GAMMtools.R")
 source("source/fig_tools.R")
 library(mgcv)
-v_path <- "/home/danilo/Documentos/mestrado_Ecologia/artigo_principal/1_to_compile_dissertacao_EM_USO/00_Resultados/"
+v_path <- "/home/danilo/Documentos/mestrado_Ecologia/artigo_principal/dados/csv_SoE/"
 # objetos comuns
-df_logOR <- read_csv(file="dados/csv/df_logOR.csv")
-df_md <- df_logOR %>% select(-Uefeito,-(pristine:denominator),-matches("(k_z|^p)")) %>% 
-  rename(logOR = itself) %>% 
-  filter(forest_succession!="capoeira") %>% 
-  mutate(contraste =  gsub("non_frag.ideal","Área per se",contraste) %>% 
-           gsub("contemp-non_frag","Frag. per se",.) %>% 
-           gsub("contemp-ideal","Frag. total",.),
-         across(c(contraste:SiteCode,forest_succession,k),factor))
-df_contrastes <- read_csv("dados/csv/taxaU/df_contrastes.csv") %>% 
-  select(SiteCode,k,ends_with("logratio")) %>% 
-  pivot_longer(cols = ends_with("logratio"),
-               names_to = "contraste",
-               values_to = "Uefeito") %>% 
-  mutate(contraste = gsub("area_logratio","Área per se",contraste) %>% 
-           gsub("frag.perse_logratio","Frag. per se",.) %>% 
-           gsub("frag.total_logratio","Frag. total",.),
-         across(SiteCode:contraste,factor))
-df_md <- inner_join(df_md,df_contrastes,by=c("SiteCode","contraste","k")) %>% 
-  relocate(Uefeito,.after="logOR") %>% 
-  mutate(k_cont = as.numeric(as.character(k)))
-saveRDS(select(df_md,contraste,SiteCode,k_cont,Uefeito,logOR),
+df_md <- readRDS(file="dados/csv_SoE/df_logOR.rds") %>% 
+  mutate(k_cont = as.numeric(as.character(k))) %>% 
+  relocate(k,.after="Uefeito")
+saveRDS(df_md,
         file=paste0(v_path,"rds/df_md.rds"))
 ##########################################################################
 ### Funções usadas para ajustar os 3 blocos de modelos: te, logU/U e k ###
@@ -74,7 +57,7 @@ f_gam_te <- \(dfi){
                       ".rds"))
   rm(l_md);gc()
 }
-lapply(split(df_md,contraste,df_md$contraste),f_gam_te)
+lapply(split(df_md,df_md$contraste),f_gam_te)
 # s(k)+s(k)|Site
 f_gam3 <- \(dfi){
   l_md <- list()
@@ -106,13 +89,11 @@ f_gam <- \(dfi,bs_type="cr"){
       s(Uefeito,bs=bs_type,m=2,id="efeito_comum") +
       s(SiteCode,bs="re"),
     data=dfi,method = "REML")
-  l_md$`1 + 1|Site` <- gam(
-    logOR ~ 1 + s(SiteCode,bs="re"),
-    data=dfi,method = "REML")
   return(l_md)
 }
 l_md_logOR <- dlply(df_md,"contraste",f_gam)
 saveRDS(l_md_logOR,file=paste0(v_path,"rds/l_md_simples_apudPedersen2019.rds"))
+rm(l_md_logOR);gc()
 ###############################################
 ############ tabela de seleção  ###############
 ###############################################
