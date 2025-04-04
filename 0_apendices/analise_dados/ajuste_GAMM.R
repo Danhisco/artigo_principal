8a63bac32ceb310eb3342eaae48107748e6b205f
Danilo Mori <dnlpmo@gmail.com>
2025-04-04 11:01:22 -0300

    ajuste_GAMM.R e rds atualizado

--- 0_apendices/analise_dados/ajuste_GAMM.R
+++ 0_apendices/analise_dados/ajuste_GAMM.R
@@ -21,61 +21,51 @@ source("source/fig_tools.R")
 library(mgcv)
 v_path <- "/home/danilo/Documentos/mestrado_Ecologia/artigo_principal/dados/csv_SoE/"
 # objetos comuns
-df_mdsumar <- readRDS(file="./dados/csv_SoE/df_mdsumar.rds")
 df_md <- readRDS(file="dados/csv_SoE/df_logOR.rds") %>% 
-  mutate(k_cont = as.numeric(as.character(k))) %>% 
+  mutate(k_cont = as.numeric(as.character(k)),
+         across(c(SiteCode,contraste,forest_succession),factor)) %>% 
   relocate(k,.after="Uefeito")
 saveRDS(df_md,
         file=paste0(v_path,"rds/df_md.rds"))
-
-
-df_p_extensoes <- read_csv("dados/csv/df_p_extensoes.csv")
-v_filter <- with(df_p_extensoes,{
-  v_i <- sapply(c(4,2,1),
-                \(x) which.min(abs(lado_km - x)))
-  return(lado_km[v_i])
-  }
-) 
-df_p <- filter(df_p_extensoes,lado_km %in% v_filter) %>% 
-  mutate(lado_km = round(lado_km,0))
-
-
-
+# df_p_extensoes <- read_csv("dados/csv/df_p_extensoes.csv")
+# v_filter <- with(df_p_extensoes,{
+#   v_i <- sapply(c(4,2,1),
+#                 \(x) which.min(abs(lado_km - x)))
+#   return(lado_km[v_i])
+#   }
+# ) 
+# df_p <- filter(df_p_extensoes,lado_km %in% v_filter) %>% 
+#   mutate(lado_km = round(lado_km,0))
 ##########################################################################
 ### Funções usadas para ajustar os 3 blocos de modelos: te, logU/U e k ###
 ##########################################################################
 ## Seguindo as especifícações de Pedersen et al. 2019 https://peerj.com/articles/6876/
 # te: tensor entre o efeito na riqueza e a capacidade de dispersão per se
-
-##############
-teste <- gam
-
-
-##############
-
 f_gam_te <- \(dfi){
   l_md <- list()
-  l_md$`te(land)|Site : gs` <- gam(
+  l_md$`te(logU/U,k)` <- gam(
     logOR ~ 
-      te(Uefeito,k_cont,
-         bs=c("cr","cr"),m=2,id = "efeito_comum") +
-      t2(Uefeito,k_cont,SiteCode,
-         bs=c("cr","cr","re"),m=2,full=TRUE,id = "efeito_sitio"),
+      te(Uefeito,k_z,
+         bs=c("cr","cr"),m=2,
+         by=forest_succession,
+         id = "fixo") +
+      s(lat,long) + 
+      t2(Uefeito,k_z,SiteCode,
+         bs=c("cr","cr","re"),
+         m=2,full=TRUE,
+         id = "random"),
     data=dfi,method = "REML")
-  l_md$`te(land) + 1|Site` <- gam(
+  l_md$`s(k)` <- gam(
     logOR ~ 
-      te(Uefeito,k_cont,
-         bs=c("cr","cr"),m=2,id = "efeito_comum") +
-      s(SiteCode,bs="re"),
-    data=dfi,method = "REML")
-  l_md$`1 + 1|Site` <- gam(
-    logOR ~ 1 + s(SiteCode,bs="re"),
+      s(k_z,bs="cr",by=forest_succession,id = "fixo") +
+      s(lat,long) + 
+      te(k_z,SiteCode,bs=c("cr","re"),id = "random"),
     data=dfi,method = "REML")
   saveRDS(l_md,
           file=paste0(v_path,"rds/l_md_",
-                      gsub("Frag. total","fragtotal",dfi$contraste[1]) %>% 
-                        gsub("Frag. per se","fragperse",.) %>% 
-                        gsub("Área per se","areaperse",.),
+                      gsub("cont-ideal","fragtotal",dfi$contraste[1]) %>% 
+                        gsub("cont-non_frag","fragperse",.) %>% 
+                        gsub("non_frag-ideal","areaperse",.),
                       ".rds"))
   rm(l_md);gc()
 }
