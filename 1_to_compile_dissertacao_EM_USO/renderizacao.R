56e560fdf3fd222cabd3e6f83fda8496cd7c128a
Danilo Mori <dnlpmo@gmail.com>
2025-07-01 12:42:52 -0300

    próximo: incluir a colagem de PDFs em renderizacao.R

--- 1_to_compile_dissertacao_EM_USO/renderizacao.R
+++ 1_to_compile_dissertacao_EM_USO/renderizacao.R
@@ -1,2 +1,2 @@
 bookdown::render_book("compilacao_integral.Rmd", "bookdown::pdf_document2")
-bookdown::render_book("compilacao_integral.Rmd", "bookdown::word_document2")
+# bookdown::render_book("compilacao_integral.Rmd", "bookdown::word_document2")
