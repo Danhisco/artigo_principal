3025fa52fa1bc3893eb275938830e3b816121cb2
Danilo Mori <dnlpmo@gmail.com>
2025-07-04 10:11:53 -0300

    correção de erros no texto (per se) e do .csl

--- 1_to_compile_dissertacao_EM_USO/renderizacao.R
+++ 1_to_compile_dissertacao_EM_USO/renderizacao.R
@@ -1,4 +1,4 @@
 bookdown::render_book("compilacao_integral.Rmd", "bookdown::pdf_document2")
 # bookdown::render_book("compilacao_integral.Rmd", "bookdown::word_document2")
-library(pdftools)
-pdf_combine(c("docs/_main.pdf""docs/_main.pdf""docs/_main.pdf", "file2.pdf"), output = "merged.pdf")
+# library(pdftools)
+# pdf_combine(c("docs/_main.pdf""docs/_main.pdf""docs/_main.pdf", "file2.pdf"), output = "merged.pdf")
