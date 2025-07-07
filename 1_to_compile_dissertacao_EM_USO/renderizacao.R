e893a18fa44f6f445edd7f033cc54b083bb6f3e3
Danilo Mori <dnlpmo@gmail.com>
2025-07-07 01:51:52 -0300

    atualização do PDF

--- 1_to_compile_dissertacao_EM_USO/renderizacao.R
+++ 1_to_compile_dissertacao_EM_USO/renderizacao.R
@@ -1,6 +1,6 @@
 bookdown::render_book("compilacao_integral.Rmd", "bookdown::pdf_document2")
 # bookdown::render_book("compilacao_integral.Rmd", "bookdown::word_document2")
 # library(pdftools)
-pdf_combine(c("capa_e_outros.pdf",
+pdftools::pdf_combine(c("capa_e_outros.pdf",
               "docs/_main.pdf"), #"~/Documentos/mestrado_Ecologia/disciplinas/BIE5716/relatorio_final.pdf"
             output = "docs/merged.pdf")
